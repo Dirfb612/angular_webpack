492f6660c7f5f0f17e2ebecf17266def9b9b8269
Dirfb612 <dianita.a2e@gmailcom>
2016-01-05 17:15:46 -0500

    some examples how to work with webpack and its loaders css

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 document.write('welcome to my app');
 console.log('app loaded');
 
-var fibonacci = require('./fibonacci.js');
-
+require('./fibonacci.js');
 require('./stiles.css');
-console.log('--- fibonacci ---');
-console.log(fibonacci);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
 // webpack.config.js
-//const webpack = require('webpack');
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
-//const extractCSS = new ExtractTextPlugin('[name].css');
-
 module.exports = {
    entry: './app.js',
    output: {
@@ -10,7 +6,6 @@ module.exports = {
    },
    module: {
       loaders: [
-
          {
             test: /\.js$/,
             loader: 'babel-loader',
@@ -20,34 +15,13 @@ module.exports = {
          },
          {
             test: /\.css$/,
-            loader: "css-loader",
-            exclude: /node_modules/
-         },
-     /*    {
-            test: /\.css$/,
-            loader: "css-loader!autoprefixer-loader"
-         },*/
-      /*   {
-            test: /\.css$/,
             loader: 'style!css',
             exclude: /node_modules/
-         },*/
-         {
-            test: /\.styl$/,
-            loader: 'style!css!stylus',
-            exclude: /node_modules/
          }
-         //{
-         //   test: /\.styl$/,
-         //   loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
-         //}
-
       ]
    },
    resolve: {
       // you can now require('file') instead of require('file.coffee')
       extensions: ['', '.js', '.json']
-   },
-
-   //plugins: [extractCSS]
+   }
 };
